512c7fab060b906dc9787ed6de6aabacab413095
peace <astonreba@gmail.com>
2020-04-16 16:33:56 +0300

    Modify file

--- submit.php
+++ submit.php
@@ -22,20 +22,5 @@ if($conn->query($sql) === TRUE){
     echo "Error: ". $sql ."<br>". $conn->error;
 }
 
-// $sql = "SELECT id, fname, lname FROM user";
-// $result = $conn->query($sql);
-
-// if($result->num_rows > 0){
-//     //output data for each row
-//     echo "<table>";
-//         while($row = $result->fetch_assoc()){
-//             echo "<tr><td> ID: ".$row["id"]. "</td></td> Name: ".$row["fname"]. " ".$row["lname"]. "</td></tr>";
-
-//         }
-//     echo "</table>";
-
-// } else {
-//     echo "0 results";
-// }
 $conn->close();
 ?>
