4ba332a014ca91e12018cc4234eaf390cdb8536b
peace <astonreba@gmail.com>
2020-04-18 18:04:44 +0300

    Modify footer content

--- index.php
+++ index.php
@@ -4,65 +4,73 @@
     <link rel="stylesheet" href="assets/style.css" />
 </head>
 <body>
-        <header>
-            <nav class="fixed-nav">
-                <div class="header-title">
-                <p><a href="#">A-Reporter<a></p>
-                </div>
-                <ul>
-                    <li><a href="#">Accidents<a></li>
-                    <li><a href="#">Report Accident<a></li>
-                    <li><a href="#">Login<a></li>
-                    <li><a href="#">Signup<a></li>
-                    <li><a href="#">Logout<a></li>
-                </ul>
-            </nav>
-        </header>
-        <main>
-            <p>
-                djnjdnjnienichiwcnibcjhinisdcibnJHVGVSHBCJBJGWEHFCVUBBBBBBBBBBBBJAHIHKkiiainsihcskckhsichihuchsincikcigsicbncgjdciiciscidcnjdcn
-                cjcbsssssssshgaknjkahxuiyhbcnuihcenjshucishiniasuxgavbuygklsxayuhaigxasyudfvwbyfuvxajhgjfajajfjabjastjrrjjnckanudyiucbaytigabah
-                ajhhgajgsjsgabhvsasjvvhtgbbakahksbcskaaaolkwnmjejeevathrisisjsssnajjjjjjbhjgagyurqqqoooepjhdjhhvjhsfcghsfghcgshcfhscshcvsgvgvsgv
-            </p>
-        </main>
-    <!-- <div>
-        <form action="submit.php" method="post">
-            First Name: <br>
-            <input type="text" name="firstname"/>
-            <br><br>
-            Last Name: <br>
-            <input type="text" name="lastname"/>
-            <br><br>
-            <input type="submit" value="Submit"/>
-        </form>
-    </div> -->
-    <footer>
-        <div class="flex-parent-ft">
-            <div class="flex-child-ft item1">
-                <ul>
-                    <li>Contact us</li>
-                    <li>0800-800 800</li>
-                </ul>
-            </div>
-            <div class="flex-child-ft item1">
-                <ul>
-                    <li>Legal</li>
-                    <li>Terms of use</li>
-                    <li>FAQs</li>
-                </ul>
-            </div>
-            <div class="flex-child-ft item1"> 
-                <ul>
-                    <li>Contact us</li>
-                    <li>0800-800 800</li>
-                </ul>
-            </div>
-            <div>
-                <div>
-                    <p>© <?= date('Y')?> All Rights Reserved.</p>
-                </div>
-            </div>
+    <div class="page-container">
+        <div  class="content-wrap">
+            <header>
+                <nav class="fixed-nav">
+                    <div class="header-title">
+                    <p><a href="#">A-Reporter<a></p>
+                    </div>
+                    <ul>
+                        <li><a href="#">Accidents<a></li>
+                        <li><a href="#">Report Accident<a></li>
+                        <li><a href="#">Login<a></li>
+                        <li><a href="#">Signup<a></li>
+                        <li><a href="#">Logout<a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <p>
+                    djnjdnjnienichiwcnibcjhinisdcibnJHVGVSHBCJBJGWEHFCVUBBBBBBBBBBBBJAHIHKkiiainsihcskckhsichihuchsincikcigsicbncgjdciiciscidcnjdcn
+                    cjcbsssssssshgaknjkahxuiyhbcnuihcenjshucishiniasuxgavbuygklsxayuhaigxasyudfvwbyfuvxajhgjfajajfjabjastjrrjjnckanudyiucbaytigabah
+                    ajhhgajgsjsgabhvsasjvvhtgbbakahksbcskaaaolkwnmjejeevathrisisjsssnajjjjjjbhjgagyurqqqoooepjhdjhhvjhsfcghsfghcgshcfhscshcvsgvgvsgv
+                </p>
+            </main>
+            <!-- <div>
+                <form action="submit.php" method="post">
+                    First Name: <br>
+                    <input type="text" name="firstname"/>
+                    <br><br>
+                    Last Name: <br>
+                    <input type="text" name="lastname"/>
+                    <br><br>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div> -->
         </div>
-    </footer>
+        <footer>
+            <div class="flex-parent-ft">
+                <div class="flex-child-ft item1">
+                    <ul>
+                        <li>Contact us</li>
+                        <li>0800-800 800</li>
+                        <li>0800-800 800</li>
+                        <li>0800-800 800</li>
+
+                    </ul>
+                </div>
+                <div class="flex-child-ft item2">
+                    <ul>
+                        <li>Legal</li>
+                        <li>Terms of use</li>
+                        <li>FAQs</li>
+                        <li>FAQs</li>
+                    </ul>
+                </div>
+                <div class="flex-child-ft item3"> 
+                    <ul>
+                        <li>Quick links</li>
+                        <li>About us</li>
+                        <li>About us</li>
+                        <li>About us</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="copyright">
+                <p>© <?= date('Y')?> All Rights Reserved.</p>
+            </div>
+        </footer>
+    </div>
 </body>
 </html>
