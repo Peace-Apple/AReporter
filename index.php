d70d67ab881f48ba251e7ae5840571c2ab3edf73
peace <astonreba@gmail.com>
2020-04-19 19:14:36 +0300

    Modify file

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
-<?php include "templates/header.php"; ?>
-    <main>
-            <p>
-                djnjdnjnienichiwcnibcjhinisdcibnJHVGVSHBCJBJGWEHFCVUBBBBBBBBBBBBJAHIHKkiiainsihcskckhsichihuchsincikcigsicbncgjdciiciscidcnjdcn
-                cjcbsssssssshgaknjkahxuiyhbcnuihcenjshucishiniasuxgavbuygklsxayuhaigxasyudfvwbyfuvxajhgjfajajfjabjastjrrjjnckanudyiucbaytigabah
-                ajhhgajgsjsgabhvsasjvvhtgbbakahksbcskaaaolkwnmjejeevathrisisjsssnajjjjjjbhjgagyurqqqoooepjhdjhhvjhsfcghsfghcgshcfhscshcvsgvgvsgv
-            </p>
-    </main>
-<?php include "templates/footer.php"; ?>
+<div>
+    <h2>Login</h2>
+    <form action="checklogin.php" method="post">
+        <label for="username">Username</label> <br>
+        <input type="text" name="username" id="username" required="required" />
+        <br><br>
+        <label for="password">Password</label><br>
+        <input type="password" name="password" id="password" required="required" />
+        <br><br>
+        <input type="submit" name="submit" value="Login"/>
+    </form>
+</div>
+
+<a href="register.php">Register Now!</a>
