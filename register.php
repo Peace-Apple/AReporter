036a793a313d3a9192aed80d3e340c5e90d241d5
peace <astonreba@gmail.com>
2020-04-19 15:15:05 +0300

    Add signup file

---
+++ register.php
@@ -0,0 +1,22 @@
+<div>
+    <form action="submit.php" method="post">
+        <label for="firstname">First Name</label> <br>
+        <input type="text" name="firstname" id="firstname" />
+        <br><br>
+        <label for="lastname">Last Name</label> <br>
+        <input type="text" name="lastname" id="lastname" />
+        <br><br>
+        <label for="email">Email Address</label><br>
+        <input type="text" name="email" id="email" />
+        <br><br>
+        <label for="contact">Contact</label><br>
+        <input type="number" name="contact" id="contact" />
+        <br><br>
+        <label for="password">Password</label><br>
+        <input type="password" name="password" id="password" />
+        <br><br>
+        <input type="submit" name="submit" value="Submit"/>
+    </form>
+</div>
+
+<a href="index.php">Back to home</a>
