7e44fcdaed619b0e870aac74f50529c49f9fc27e
peace <astonreba@gmail.com>
2020-04-19 15:16:35 +0300

    Separate header and footer into separate files

---
+++ templates/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="assets/style.css" />
+</head>
+<body>
+    <div  class="content-wrap">
+        <div class="main">
+        <header>
+            <nav class="fixed-nav">
+                <div class="header-title">
+                <p><a href="#">A-Reporter<a></p>
+                </div>
+                <ul>
+                    <li><a href="#">Accidents<a></li>
+                    <li><a href="#">Report Accident<a></li>
+                    <li><a href="#">Login<a></li>
+                    <li><a href="#">Signup<a></li>
+                    <li><a href="#">Logout<a></li>
+                </ul>
+            </nav>
+        </header>
