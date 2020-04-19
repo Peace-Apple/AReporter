6597f4f2599d2392c4bd307113c265d681753a6a
peace <astonreba@gmail.com>
2020-04-19 16:52:43 +0300

    Add login file

---
+++ login.php
@@ -0,0 +1,14 @@
+<div>
+    <h2>Login</h2>
+    <form action="checklogin.php" method="post">
+        <label for="username">Username</label> <br>
+        <input type="text" name="username" id="username" required="required" />
+        <br><br>
+        <label for="password">Password</label><br>
+        <input type="password" name="password" id="password" required="required" />
+        <br><br>
+        <input type="submit" name="submit" value="Login"/>
+    </form>
+</div>
+
+<a href="index.php">Back</a>
