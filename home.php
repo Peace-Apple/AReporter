b99e875e2c2b3c076e9cbd3c45f1d723920a1b78
peace <astonreba@gmail.com>
2020-04-19 19:15:04 +0300

    Add home page

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php include "templates/header.php"; ?>
+<?php
+   session_start(); //starts the session
+   if($_SESSION['user']){ // checks if the user is logged in  
+   }
+   else{
+      header("location: index.php"); // redirects if user is not logged in
+   }
+   $user = $_SESSION['user']; //assigns user value
+?>
+    <main>
+        <p>
+            There are no reported accidents at the moment...
+        </p>
+    </main>
+<?php include "templates/footer.php"; ?>
