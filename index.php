ddde4dcd15d3239855b4c5252d039bc4b8cb6461
peace <astonreba@gmail.com>
2020-04-19 15:15:36 +0300

    Modify file

--- index.php
+++ index.php
@@ -1,76 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" href="assets/style.css" />
-</head>
-<body>
-        <div  class="content-wrap">
-            <div class="main">
-            <header>
-                <nav class="fixed-nav">
-                    <div class="header-title">
-                    <p><a href="#">A-Reporter<a></p>
-                    </div>
-                    <ul>
-                        <li><a href="#">Accidents<a></li>
-                        <li><a href="#">Report Accident<a></li>
-                        <li><a href="#">Login<a></li>
-                        <li><a href="#">Signup<a></li>
-                        <li><a href="#">Logout<a></li>
-                    </ul>
-                </nav>
-            </header>
-            <main>
-                <p>
-                    djnjdnjnienichiwcnibcjhinisdcibnJHVGVSHBCJBJGWEHFCVUBBBBBBBBBBBBJAHIHKkiiainsihcskckhsichihuchsincikcigsicbncgjdciiciscidcnjdcn
-                    cjcbsssssssshgaknjkahxuiyhbcnuihcenjshucishiniasuxgavbuygklsxayuhaigxasyudfvwbyfuvxajhgjfajajfjabjastjrrjjnckanudyiucbaytigabah
-                    ajhhgajgsjsgabhvsasjvvhtgbbakahksbcskaaaolkwnmjejeevathrisisjsssnajjjjjjbhjgagyurqqqoooepjhdjhhvjhsfcghsfghcgshcfhscshcvsgvgvsgv
-                </p>
-            </main>
-            <!-- <div>
-                <form action="submit.php" method="post">
-                    First Name: <br>
-                    <input type="text" name="firstname"/>
-                    <br><br>
-                    Last Name: <br>
-                    <input type="text" name="lastname"/>
-                    <br><br>
-                    <input type="submit" value="Submit"/>
-                </form>
-            </div> -->
-        </div>
-        </div>
-        <footer>
-            <div class="flex-parent-ft">
-                <div class="flex-child-ft item1">
-                    <h4>Contact us</h4>
-                    <ul>
-                        <li><a>0800-800 800</a></li>
-                        <li><a>Facebook</a></li>
-                        <li><a>Twitter</a></li>
-
-                    </ul>
-                </div>
-                <div class="flex-child-ft item2">
-                    <h4>Legal</h4>
-                    <ul>
-                        <li><a>Terms of use</a></li>
-                        <li><a>Privacy policy</a></li>
-                        <li><a>FAQs</a></li>
-                    </ul>
-                </div>
-                <div class="flex-child-ft item3"> 
-                    <h4>Quick links</h4>
-                    <ul>
-                        <li><a>About us</a></li>
-                        <li><a>Contact us</a></li>
-                        <li><a>Report Incident</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="copyright">
-                <p>© <?= date('Y')?> All Rights Reserved.</p>
-            </div>
-        </footer>
-</body>
-</html>
+<?php include "templates/header.php"; ?>
+    <main>
+            <p>
+                djnjdnjnienichiwcnibcjhinisdcibnJHVGVSHBCJBJGWEHFCVUBBBBBBBBBBBBJAHIHKkiiainsihcskckhsichihuchsincikcigsicbncgjdciiciscidcnjdcn
+                cjcbsssssssshgaknjkahxuiyhbcnuihcenjshucishiniasuxgavbuygklsxayuhaigxasyudfvwbyfuvxajhgjfajajfjabjastjrrjjnckanudyiucbaytigabah
+                ajhhgajgsjsgabhvsasjvvhtgbbakahksbcskaaaolkwnmjejeevathrisisjsssnajjjjjjbhjgagyurqqqoooepjhdjhhvjhsfcghsfghcgshcfhscshcvsgvgvsgv
+            </p>
+    </main>
+<?php include "templates/footer.php"; ?>
