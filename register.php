c7cbde86bcd75b7274b41117aa9b44e89613c0ac
peace <astonreba@gmail.com>
2020-04-19 16:53:54 +0300

    Modify register file

--- register.php
+++ register.php
@@ -1,21 +1,39 @@
+<?php include "connection.php"; ?>
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $name = $_POST['username'];
+        $email = $_POST['email'];
+        $contact = $_POST['contact'];
+        $pass = $_POST['password'];
+
+        $sql = "INSERT INTO `user` (`username`, `email`, `contact`, `password`) VALUES ('$name', '$email', '$contact', '$pass')";
+
+
+        if($conn->query($sql) === TRUE){
+            echo "New record created successfully";
+        } else {
+            echo "Error: ". $sql ."<br>". $conn->error;
+        }
+        $conn->close();
+    }
+?>
+
 <div>
-    <form action="submit.php" method="post">
-        <label for="firstname">First Name</label> <br>
-        <input type="text" name="firstname" id="firstname" />
-        <br><br>
-        <label for="lastname">Last Name</label> <br>
-        <input type="text" name="lastname" id="lastname" />
+    <h2>Register</h2>
+    <form action="register.php" method="post">
+        <label for="username">Username</label> <br>
+        <input type="text" name="username" id="username" required="required" />
         <br><br>
         <label for="email">Email Address</label><br>
-        <input type="text" name="email" id="email" />
+        <input type="text" name="email" id="email" required="required" />
         <br><br>
         <label for="contact">Contact</label><br>
-        <input type="number" name="contact" id="contact" />
+        <input type="number" name="contact" id="contact" required="required" />
         <br><br>
         <label for="password">Password</label><br>
-        <input type="password" name="password" id="password" />
+        <input type="password" name="password" id="password" required="required" />
         <br><br>
-        <input type="submit" name="submit" value="Submit"/>
+        <input type="submit" name="submit" value="Register"/>
     </form>
 </div>
 
