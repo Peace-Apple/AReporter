2649013193935ec19d46a68708a6934d5a4692a6
peace <astonreba@gmail.com>
2020-04-19 15:17:11 +0300

    Modify query to add a new user

--- submit.php
+++ submit.php
@@ -1,6 +1,9 @@
 <?php
 $first = $_POST['firstname'];
 $last = $_POST['lastname'];
+$email = $_POST['email'];
+$contact = $_POST['contact'];
+$pass = $_POST['password'];
 $servername = 'localhost';
 $username = 'root';
 $password = '';
@@ -14,7 +17,7 @@ if($conn->connect_error){
     die("Connection failed: ". $conn->connect_error);
 }
 echo "Connected successfully."; 
-$sql = "INSERT INTO `user` (`fname`, `lname`) VALUES ('$first','$last')";
+$sql = "INSERT INTO `user` (`fname`, `lname`, `email`, `contact`, `password`) VALUES ('$first','$last', '$email', '$contact', '$pass')";
 
 if($conn->query($sql) === TRUE){
     echo "New record created successfully";
