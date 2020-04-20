7f50d8cfb98de9723469e9ee3a220aa7a82f1390
peace <astonreba@gmail.com>
2020-04-21 00:17:57 +0300

    Modify file

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <head>
     <link rel="stylesheet" href="assets/style.css" />
-    <style>
-        body { 
-            background-image: url('images/mag.jpeg'); 
-        }
-    </style>
 </head>
-<div class="auth-forms">
-    <div class="login">
-        <h2>Login</h2>
-        <form action="checklogin.php" method="post">
-            <input type="text" name="username" id="username" placeholder="Enter Username" required="required" />
-            <br><br>
-            <input type="password" name="password" id="password" placeholder=" Enter Password" required="required" />
-            <br><br>
-            <input type="submit" name="submit" value="Login"/>
-        </form>
-        <p><a href="register.php">No account? Register Now!</a></p>
+<body class="auth">
+    <div class="auth-forms">
+        <div class="login">
+            <h2>Login</h2>
+            <form action="checklogin.php" method="post">
+                <input type="text" name="username" id="username" placeholder="Enter Username" required="required" />
+                <br><br>
+                <input type="password" name="password" id="password" placeholder=" Enter Password" required="required" />
+                <br><br>
+                <input type="submit" name="submit" value="Login"/>
+            </form>
+            <p><a href="register.php">No account? Register Now!</a></p>
+        </div>
     </div>
-</div>
+</body>
 
 
