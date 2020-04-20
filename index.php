7f3186f25777ea58c895ff9b1740f5e7a417f8bf
peace <astonreba@gmail.com>
2020-04-20 21:53:14 +0300

    Style login form

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <head>
     <link rel="stylesheet" href="assets/style.css" />
+    <style>
+        body { 
+            background-image: url('images/mag.jpeg'); 
+        }
+    </style>
 </head>
-<div class="login">
-    <h2>Login</h2>
-    <form action="checklogin.php" method="post">
-        <label for="username">Username</label> <br>
-        <input type="text" name="username" id="username" required="required" />
-        <br><br>
-        <label for="password">Password</label><br>
-        <input type="password" name="password" id="password" required="required" />
-        <br><br>
-        <input type="submit" name="submit" value="Login"/>
-    </form>
+<div class="auth-forms">
+    <div class="login">
+        <h2>Login</h2>
+        <form action="checklogin.php" method="post">
+            <input type="text" name="username" id="username" placeholder="Enter Username" required="required" />
+            <br><br>
+            <input type="password" name="password" id="password" placeholder=" Enter Password" required="required" />
+            <br><br>
+            <input type="submit" name="submit" value="Login"/>
+        </form>
+        <p><a href="register.php">No account? Register Now!</a></p>
+    </div>
 </div>
 
-<a href="register.php">Register Now!</a>
+
