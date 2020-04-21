2168b81653717469ba11c48cb253714d0f30ee29
peace <astonreba@gmail.com>
2020-04-21 22:40:19 +0300

    Modify file

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     <link rel="stylesheet" href="assets/style.css" />
 </head>
 <body class="auth">
+    <h3>Welcome to A-REPORTER, report a accident today!</h3>
     <div class="auth-forms">
         <div class="login">
             <h2>Login</h2>
