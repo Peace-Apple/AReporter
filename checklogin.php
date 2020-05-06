68b6516d5882f121a2963d50930eeeeef26908a4
peace <astonreba@gmail.com>
2020-05-06 19:50:46 +0300

    Add check to see whether user is admin and redirect accordingly

--- checklogin.php
+++ checklogin.php
@@ -7,21 +7,28 @@
     $query = "SELECT * from user WHERE username='$username'"; //Query the users table if there are matching rows equal to $username
     $result = $conn->query($query);
     $exists = mysqli_num_rows($result); //Checks if username exists
-    $table_users = "";
+    $table_username = "";
     $table_password = "";
+    
     if($exists > 0) //IF there are no returning rows or no existing username
     {
-        while($row = mysqli_fetch_assoc($result)) //display all rows from query
+        while ($row = mysqli_fetch_assoc($result)) //display all rows from query
         {
-            $table_users = $row['username']; // the first username row is passed on to $table_users, and so on until the query is finished
-            $table_password = $row['password']; // the first password row is passed on to $table_users, and so on until the query is finished
+            $table_username = $row['username'];
+            $table_password = $row['password']; 
+            $admin = $row['admin']; 
         }
-        if(($username == $table_users) && ($password == $table_password)) // checks if there are any matching fields
+        if (($username == $table_username) && ($password == $table_password)) // checks if there are any matching fields
         {
             if($password == $table_password)
             {
                 $_SESSION['user'] = $username; //set the username in a session. This serves as a global variable
-                header("location: home.php"); // redirects the user to the authenticated home page
+
+                if ($admin) {
+                    header("location: admin.php"); // redirects the admin to the authenticated admin page
+                } else {
+                    header("location: home.php"); // redirects the user to the authenticated home page
+                }
             }
                 
         }
