be14cfe2a2e0d19ce798102a880fa18b66fcb8e3
peace <astonreba@gmail.com>
2020-05-06 20:30:17 +0300

    Modify file to separate admin and user views

--- templates/header.php
+++ templates/header.php
@@ -10,17 +10,37 @@
     </style>
 </head>
 <body class="mainbody">
+    <?php
+        session_start(); //starts session
+        if($_SESSION['user']){
+
+        }
+        $user = $_SESSION['user'];
+    ?>
     <div  class="content-wrap">
         <div class="main">
         <header>
             <nav class="fixed-nav">
-                <div class="header-title">
-                <h2><a href="home.php">A-Reporter<a></h2>
-                </div>
-                <ul>
-                    <li><a href="cases.php">Incidents<a></li>
-                    <li><a href="report.php">Report Accident<a></li>
-                    <li><a href="logout.php">Logout<a></li>
-                </ul>
+                <?php 
+                if ($user != 'Deyo') {
+                    echo '<div class="header-title">
+                            <h2><a href="home.php">A-Reporter<a></h2>
+                        </div>
+                    <ul>
+                        <li><a href="cases.php">Incidents<a></li>
+                        <li><a href="report.php">Report Accident<a></li>
+                        <li><a href="logout.php">Logout<a></li>
+                    </ul>';
+                } else {
+                    echo'<div class="header-title">
+                            <h2><a href="cases.php">A-Reporter<a></h2>
+                         </div>
+                    <ul>
+                        <li><a href="cases.php">Incidents<a></li>
+                        <li><a href="logout.php">Logout<a></li>
+                    </ul>';
+                }
+                ?>
+                
             </nav>
         </header>
