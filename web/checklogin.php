eafe841d373e7f814dc837ec797e1ca9aeaa815a
peace <astonreba@gmail.com>
2020-07-05 22:06:11 +0300

    Make changes in file

--- web/checklogin.php
+++ web/checklogin.php
@@ -1,5 +1,6 @@
-<?php include "../connection.php"; ?>
-<?php
+<?php 
+    include "../connection.php";
+
     session_start();
     $username = $_POST['username'];
     $password = $_POST['password'];
