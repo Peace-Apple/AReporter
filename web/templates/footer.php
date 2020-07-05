a0c9d6911c13eb030c6d5a26beb6769e357fde21
peace <astonreba@gmail.com>
2020-07-05 21:30:47 +0300

    Modify file

--- web/templates/footer.php
+++ web/templates/footer.php
@@ -1,36 +1,36 @@
 </div>
-        </div>
-        <footer>
-            <div class="flex-parent-ft">
-                <div class="flex-child-ft item1">
-                    <h4>Contact us</h4>
-                    <ul>
-                        <li><a>0800-800 800</a></li>
-                        <li><a>Facebook</a></li>
-                        <li><a>Twitter</a></li>
+    </div>
+    <footer>
+        <div class="flex-parent-ft">
+            <div class="flex-child-ft item1">
+                <h4>Contact us</h4>
+                <ul>
+                    <li><a>0800-800 800</a></li>
+                    <li><a>Facebook</a></li>
+                    <li><a>Twitter</a></li>
 
-                    </ul>
-                </div>
-                <div class="flex-child-ft item2">
-                    <h4>Legal</h4>
-                    <ul>
-                        <li><a>Terms of use</a></li>
-                        <li><a>Privacy policy</a></li>
-                        <li><a>FAQs</a></li>
-                    </ul>
-                </div>
-                <div class="flex-child-ft item3"> 
-                    <h4>Quick links</h4>
-                    <ul>
-                        <li><a>About us</a></li>
-                        <li><a>Contact us</a></li>
-                        <li><a href="report.php">Report Incident</a></li>
-                    </ul>
-                </div>
+                </ul>
             </div>
-            <div class="copyright">
-                <p>© <?= date('Y')?> All Rights Reserved.</p>
+            <div class="flex-child-ft item2">
+                <h4>Legal</h4>
+                <ul>
+                    <li><a>Terms of use</a></li>
+                    <li><a>Privacy policy</a></li>
+                    <li><a>FAQs</a></li>
+                </ul>
             </div>
-        </footer>
+            <div class="flex-child-ft item3"> 
+                <h4>Quick links</h4>
+                <ul>
+                    <li><a>About us</a></li>
+                    <li><a>Contact us</a></li>
+                    <li><a href="report.php">Report Incident</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="copyright">
+            <p>© <?= date('Y')?> All Rights Reserved.</p>
+        </div>
+    </footer>
 </body>
 </html>
